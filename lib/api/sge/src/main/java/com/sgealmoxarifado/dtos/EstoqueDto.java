38b9d4406c76311983f564db026db9a559b325b8
NelsonFranco <nelsonfranco431@gmail.com>
2026-05-22 18:27:32 -0300

    Classes de Estoque descartadas, reestruturação final do código da API, CORS configurado para teste

--- lib/api/sge/src/main/java/com/sgealmoxarifado/dtos/EstoqueDto.java
+++
@@ -1,9 +0,0 @@
-package com.sgealmoxarifado.dtos;
-
-public record EstoqueDto( 
-     String estNome,
-     int quantMax,
-     String local,
-     String responsavel) {
-
-}
--- lib/api/sge/src/main/java/com/sgealmoxarifado/entitys/model/UserCommon.java
+++
@@ -1,5 +0,0 @@
-package com.sgealmoxarifado.entitys.model;
-
-public class UserCommon {
-
-}
